0a0e44472ce624ec76cb963673710457bba19bc1
Slava <SlavaShensev@gmail.com>
2021-01-17 22:07:33 +0200

    'add_dialogs_21'

--- src/component/Dialogs/Dialogs.tsx
+++ src/component/Dialogs/Dialogs.tsx
@@ -3,8 +3,41 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.items}>
+                    Slava
+                </div>
+                <div className={s.items}>
+                    Sasha
+                </div>
+                <div className={s.items}>
+                    Elena
+                </div>
+                <div className={s.items}>
+                    Antony
+                </div>
+                <div className={s.items}>
+                    Tom
+                </div>
+                <div className={s.items}>
+                    Olya
+                </div>
+                <div className={s.items}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello!
+                </div>
+                <div className={s.message}>
+                    Tony! Where are you go?
+                </div>
+                <div className={s.message}>
+                    Mom, i love you))
+                </div>
+            </div>
         </div>
     )
 }
