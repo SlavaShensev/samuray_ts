8c4aaa661e92e128c1c94b682eae8f35ff0f33fe
Slava <SlavaShensev@gmail.com>
2021-01-21 16:36:29 +0200

    23_TS_Dialogs_and_MyPosts

--- src/component/Dialogs/Dialogs.tsx
+++ src/component/Dialogs/Dialogs.tsx
@@ -3,16 +3,17 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 type dialogItemType = {
-    id: string | number,
+    id: number,
     name: string,
 }
 
-type message = {
+type messagesType = {
+    id: number
     message: string
 }
 
 const DialogItem = (props: dialogItemType) => {
-    const path = '/dialogs/' + props.id
+    const path = `/dialogs/${props.id}`
     return (
         <div className={s.items + ' ' + s.active}>
             <NavLink to={path}>
@@ -22,7 +23,7 @@ const DialogItem = (props: dialogItemType) => {
     )
 }
 
-const Message = (props: message) => {
+const Message = (props: messagesType) => {
     return (
         <div>
             {props.message}
@@ -31,21 +32,36 @@ const Message = (props: message) => {
 }
 
 const Dialogs = () => {
+    const dialogsData: Array<dialogItemType> = [
+        {id: 1, name: 'Slava'},
+        {id: 2, name: 'Sasha'},
+        {id: 3, name: 'Elena'},
+        {id: 4, name: 'Antony'},
+        {id: 5, name: 'Tom'},
+        {id: 6, name: 'Olya'},
+        {id: 7, name: 'Sveta'},
+    ]
+    const messagesData: Array<messagesType> = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'Slava! Where are you go?'},
+        {id: 3, message: 'Are you read React?'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-                <DialogItem id={1} name={'Slava'}/>
-                <DialogItem id={2} name={'Sasha'}/>
-                <DialogItem id={3} name={'Elena'}/>
-                <DialogItem id={4} name={'Antony'}/>
-                <DialogItem id={5} name={'Tom'}/>
-                <DialogItem id={6} name={'Olya'}/>
-                <DialogItem id={7} name={'Sveta'}/>
+                <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+                <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
+                <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
+                <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
+                <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}/>
+                <DialogItem id={dialogsData[5].id} name={dialogsData[5].name}/>
+                <DialogItem id={dialogsData[6].id} name={dialogsData[6].name}/>
             </div>
             <div className={s.messages}>
-                <Message message={'Hello!'}/>
-                <Message message={'Slava! Where are you go?'}/>
-                <Message message={'Are you read React?'}/>
+                <Message message={messagesData[0].message} id={messagesData[0].id}/>
+                <Message message={messagesData[1].message} id={messagesData[1].id}/>
+                <Message message={messagesData[2].message} id={messagesData[2].id}/>
             </div>
         </div>
     )
--- src/component/Profile/MyPosts/MyPosts.tsx
+++ src/component/Profile/MyPosts/MyPosts.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import Post from "./Post/Post";
 
+export type postDataType = {
+    id: number,
+    message: string,
+    likesCount: number,
+}
+
 const MyPosts = () => {
+    const postData: Array<postDataType> = [
+        {
+            id: 1,
+            message: 'Hello, how are you?',
+            likesCount: 13,
+        },
+        {
+            id: 2,
+            message: 'It is my first post',
+            likesCount: 34,
+        },
+    ]
     return (
         <div>
             My post
@@ -11,7 +29,14 @@ const MyPosts = () => {
             <textarea></textarea>
             <button>Add post</button>
             <div>
-                <Post />
+                <Post id={postData[0].id}
+                      message={postData[0].message}
+                      likesCount={postData[0].likesCount}
+                />
+                <Post id={postData[1].id}
+                      message={postData[1].message}
+                      likesCount={postData[1].likesCount}
+                />
             </div>
         </div>
     )
